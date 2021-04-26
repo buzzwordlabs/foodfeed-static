a27ecf32f72a12cdafe6c21e8221aee96b12123a
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2021-04-25 20:22:12 -0700

    public commit

---
+++ precache-manifest.63298907eff99b82c664f19ad8ad858d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe195e3b93eccfa06ccccc692f69465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3ee872369b55f6a75f5",
+    "url": "/static/css/2.6ef2c208.chunk.css"
+  },
+  {
+    "revision": "5b62e543ee6116168adb",
+    "url": "/static/css/main.8b90b64d.chunk.css"
+  },
+  {
+    "revision": "d3ee872369b55f6a75f5",
+    "url": "/static/js/2.e94a4fa4.chunk.js"
+  },
+  {
+    "revision": "ed6d44160be3da7ea632241f05f7ba30",
+    "url": "/static/js/2.e94a4fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b62e543ee6116168adb",
+    "url": "/static/js/main.855d351c.chunk.js"
+  },
+  {
+    "revision": "1d9f75eb314fa3da8381",
+    "url": "/static/js/runtime-main.0c359be8.js"
+  },
+  {
+    "revision": "d2df1968dc30a0c6b689c325d95189d7",
+    "url": "/static/media/ani-profile-pic.d2df1968.jpg"
+  },
+  {
+    "revision": "d0558d91063038236b60e3ef71fdc1fd",
+    "url": "/static/media/app-store-icon.d0558d91.svg"
+  },
+  {
+    "revision": "771f87f7c17e4ca0d6553617e05fa459",
+    "url": "/static/media/call-screenshot.771f87f7.png"
+  },
+  {
+    "revision": "980b5f95c69ecd570d004d50d29f9643",
+    "url": "/static/media/ethan-profile-pic-2.980b5f95.jpg"
+  },
+  {
+    "revision": "92660e029abbae43c3ed835685b5105c",
+    "url": "/static/media/github.92660e02.svg"
+  },
+  {
+    "revision": "5b33fc42049482eebe41defc38e7a023",
+    "url": "/static/media/linkedin.5b33fc42.svg"
+  },
+  {
+    "revision": "ca9da7438fc256ba35907002299907ab",
+    "url": "/static/media/logo-rectangle-transparent.ca9da743.png"
+  },
+  {
+    "revision": "7ced965efa19fa4897fd5282eeecf936",
+    "url": "/static/media/logo-rectangle.7ced965e.png"
+  },
+  {
+    "revision": "f933ae7d894475cf7176bef11bf56a53",
+    "url": "/static/media/mockup.f933ae7d.png"
+  },
+  {
+    "revision": "e3ea015c98c4f1809ad62d0ef9c14a88",
+    "url": "/static/media/play-store-icon.e3ea015c.svg"
+  },
+  {
+    "revision": "dd184917562f9fa0bc61a038222aedfe",
+    "url": "/static/media/twitter.dd184917.svg"
+  }
+]);
